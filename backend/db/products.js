fc75c71b6f1128f68e1cb7ede26efa0e8a934684
Chu Song <chuchusong23@gmail.com>
2018-08-01 23:02:57 -0700

    Update product and user schema.

--- backend/db/products.js
+++ backend/db/products.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let ProductSchema = new Schema({
-    name: String,
-    description: String,
-    price: Number
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true },
+    imagePath: { type: String, required: true }
 });
 
-export default mongoose.model("Products", ProductSchema);
+module.exports = mongoose.model("Products", ProductSchema);
--- backend/db/users.js
+++ backend/db/users.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-    username: String,
-    firstname: String,
-    lastname: String,
-    password: String,
-    email: String,
-    street: String,
-    state: String,
-    zipcode: String,
-    products: Array
+    username: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    street: { type: String },
+    city: { type: String },
+    state:{ type: String },
+    zipcode: { type: String },
+    products: { type: Array }
 });
 
-export default mongoose.model("Users", UserSchema);
+module.exports = mongoose.model("Users", UserSchema);
