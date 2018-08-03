f7e665ab0fdc6ee0e654fed8717f23b299647a1e
Chu Song <chuchusong23@gmail.com>
2018-08-03 16:44:10 -0700

    Add routs for different kinds of nails.

--- backend/server/routes/products.js
+++ backend/server/routes/products.js
@@ -13,4 +13,34 @@ router.get("/", (req, res) => {
         })
 });
 
+router.get("/press-on", (req, res) => {
+    Products.find({category: "press-on"})
+        .then(products => {
+            res.json(products);
+        })
+        .catch(err => {
+            res.json("This is an error: ", err);
+        })
+});
+
+router.get("/glue-on", (req, res) => {
+    Products.find({category: "glue-on"})
+        .then(products => {
+            res.json(products);
+        })
+        .catch(err => {
+            res.json("This is an error: ", err);
+        })
+});
+
+router.get("/nude-french", (req, res) => {
+    Products.find({category: "nude french"})
+        .then(products => {
+            res.json(products);
+        })
+        .catch(err => {
+            res.json("This is an error: ", err);
+        })
+});
+
 module.exports = router;
