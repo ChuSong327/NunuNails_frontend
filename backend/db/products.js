479ca6a01cca465c47e849fda15213e0280079fa
Chu Song <chuchusong23@gmail.com>
2018-08-09 13:24:37 -0700

    Change the data type of price from string to number.

--- backend/db/products.js
+++ backend/db/products.js
@@ -6,7 +6,7 @@ let ProductSchema = new Schema({
     name: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
     imagePath: { type: String, required: true }
 });
 
