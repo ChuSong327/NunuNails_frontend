7a21d5941eed73f9d79be1711a8e459c18479cf9
Chu Song <chuchusong23@gmail.com>
2018-08-09 13:27:10 -0700

    Add get shopping cart and total price functions to dynamically display all products in shopping cart and the total price of all products. Edit style of the cart page.

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Product {
+  _id: string;
+  name: string;
+  category: string;
+  description: string;
+  price: string;
+}
+
+interface Products {
+  product: Product;
+  quantity: string;
+}
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -9,7 +22,30 @@ export class CartComponent implements OnInit {
 
   constructor() { }
 
+  /** Define products stored in localStorage */
+  products: Products[] = [];
+
+  totalPrice: number;
+
+  /** Get products in the shopping cart */
+  private get getCartFromStorage(): Products[] {
+    if(localStorage.length) {
+      return JSON.parse(localStorage.getItem("cart"));
+    }
+  }
+
+  /** Get the total price of all products in the shopping cart */
+  private getTotalPrice(products)  {
+    let total = 0;
+    for(const product of products) {
+      total = Number(total) + Number(product.product.price * product.quantity);
+    }
+    return total;
+  }
+
   ngOnInit() {
+   this.products = this.getCartFromStorage;
+   this.totalPrice = this.getTotalPrice(this.products);
   }
 
 }
